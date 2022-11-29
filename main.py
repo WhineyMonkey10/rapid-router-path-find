3d2b2950f2c422814a67c2069fb961c6a4025654
WhineyMonkey10 <71403599+WhineyMonkey10@users.noreply.github.com>
2022-11-29 14:49:40 +0100

    Update main.py

--- main.py
+++ main.py
@@ -16,6 +16,9 @@ class movement():
       my_van.turn_right()
   def auto_move():
     while True:
+      if my_van.at_destination():
+        print("Arrived")
+        break
       if my_van.is_road_forward():
         movement.move_forward(1)
       elif my_van.is_road_left():
@@ -24,9 +27,7 @@ class movement():
         movement.right(1)
       elif my_van.at_dead_end():
         my_van.turn_around()
-      elif my_van.at_destination():
-        print("Arrived")
-        break
+
       elif my_van.at_red_traffic_light():
           my_van.wait()
           while True:
