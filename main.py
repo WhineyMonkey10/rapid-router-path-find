dcdf863e89d3a669dd02e6dc658f8406b5d5ae46
WhineyMonkey10 <71403599+WhineyMonkey10@users.noreply.github.com>
2022-12-03 14:34:11 +0100

    Reverted

--- main.py
+++ main.py
@@ -15,63 +15,27 @@ class movement():
     elif direction == right:
       for i in range(num):
         my_van.turn_right()
-  def auto_move(priority):
-    if priority == Null:
-      while True:
-        if my_van.at_destination():
-          print("Arrived")
-          break
-        if my_van.is_road_forward():
-          movement.move_forward(1)
-        elif my_van.is_road_left():
-          movement.turn(1, left)
-        elif my_van.is_road_right():
-          movement.turn(1, right)
-        elif my_van.at_dead_end():
-          my_van.turn_around()
+  def auto_move():
+    while True:
+      if my_van.at_destination():
+        print("Arrived")
+        break
+      if my_van.is_road_forward():
+        movement.move_forward(1)
+      elif my_van.is_road_left():
+        movement.turn(1, left)
+      elif my_van.is_road_right():
+        movement.turn(1, right)
+      elif my_van.at_dead_end():
+        my_van.turn_around()
 
-        elif my_van.at_red_traffic_light():
-            my_van.wait()
-            while True:
-              my_van.at_green_traffic_light()
-              break
-      elif priority == "left":
-        while True:
-          if my_van.at_destination():
-            print("Arrived")
+      elif my_van.at_red_traffic_light():
+          my_van.wait()
+          while True:
+            my_van.at_green_traffic_light()
             break
-          if my_van.is_road_forward():
-            movement.move_forward(1)
-          elif my_van.is_road_left():
-            movement.turn(1, left)
-            if my_van.is_road_forward():
-              movement.move_forward(1)
-            elif my_van.is_road_left():
-              movement.turn(1, left)
-            
-            elif my_van.is_road_right():
-              movement.turn(1, right)
-            elif my_van.at_dead_end():
-              my_van.turn_around()
+       
+    
+       
 
-            elif my_van.at_red_traffic_light():
-                my_van.wait()
-                while True:
-                  my_van.at_green_traffic_light()
-                  break
-          elif my_van.is_road_right():
-            movement.turn(1, right)
-          elif my_van.at_dead_end():
-            my_van.turn_around()
-
-          elif my_van.at_red_traffic_light():
-              my_van.wait()
-              while True:
-                my_van.at_green_traffic_light()
-                break
-        
-      
-#Priorities, if it's a map where the ending is not a dead end, or the roads are a "trick" you're able to prioritize which way the van turns. In development cause I'm only coding this in like 5 minutes on GitHub.
-priority = Null #most likely only works for left, not sure though
-
-movement.auto_move(priority)
+movement.auto_move()
